16469a8790d57b71c0278f1e32e239764ae0d65c
Michelle Munksgard <design@michellemunksgard.com>
2014-11-06 17:12:33 -0500

    Arrays

---
+++ assignment [Values_Variables]/arrays/js/script.js
@@ -0,0 +1,11 @@
+//Arrays - holds collections
+
+var muppetNames = ["Kermit", "Miss Piggy"];// 0,1,2,3
+
+
+muppetNames [1] = "Rizzo"; //change index #1 to Rizzo
+muppetNames [2] = "Fozzy";
+
+var num = 2;
+
+console.log(muppetNames[num]); //lets me see whats in index #1
