9b744fdc6b159763cb98783d7d0476bcde1777e3
Michelle Munksgard <design@michellemunksgard.com>
2014-11-06 16:49:35 -0500

    Strings and booleans

---
+++ assignment [Values_Variables]/string_boolean/js/script.js
@@ -0,0 +1,9 @@
+//Variable of Different Types, Numbers, Booleans and Strings
+
+var age = 30; //Number variable
+
+var isStudent = true; //Boolean Variable
+
+var firstName = "Michelle";
+
+var phrase = 'I don\'t know. \n You\'ll have to speak with the professor'; //n = new line (line break)
