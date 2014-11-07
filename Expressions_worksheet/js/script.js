39648b9d7945f2edbd105ab7f596c14df5cc4818
Michelle Munksgard <design@michellemunksgard.com>
2014-11-06 21:45:46 -0500

    Discount

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -24,3 +24,36 @@ slicesPerPerson = (noOfPizzas * slicesPerPizza) / noOfPeople;
 console.log("Each person ate " + slicesPerPerson + " slices of pizza at the party.");
 
 
+//Slice of Pie Part 2
+//Determine number of pieces Sparky gets
+
+var totalSlices = slicesPerPizza * noOfPizzas;
+var sparkyPizza = totalSlices - (slicesPerPerson * noOfPeople);
+
+console.log(totalSlices);
+console.log("Sparky got " + sparkyPizza + " slices of pizza.");
+
+
+//Average shopping bill
+//Find total amount spend on groceries and average weekly grocery spending
+
+var eachWeek = [54, 44, 83, 47, 38];
+var total = (eachWeek[0] + eachWeek[1] + eachWeek[2] + eachWeek[3] + eachWeek[4]);
+var average = total/5;
+
+console.log("You have spent a total of $" + total + " on groceries over 5 weeks. That is an average of $" + average + " per week" );
+
+
+//Discounts
+//Calculate the price with or without tax
+
+var price = 15.99;
+var discount = 30;
+var description = "Dog Treats"
+var salesTax = 6;
+
+var withoutTax = price - (price * (discount/100));
+var withTax = withoutTax + (withoutTax * (salesTax/100));
+
+console.log(withoutTax);
+console.log(withTax);
