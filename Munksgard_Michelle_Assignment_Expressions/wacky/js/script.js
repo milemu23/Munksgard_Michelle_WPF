b5c69e29129bc1e265e19ea46d038665dd1a5e41
Michelle Munksgard <design@michellemunksgard.com>
2014-11-06 22:59:22 -0500

    Wacky Work in Progress

--- Munksgard_Michelle_Assignment_Expressions/wacky/js/script.js
+++ Munksgard_Michelle_Assignment_Expressions/wacky/js/script.js
@@ -1,6 +1,16 @@
 //Michelle Munksgard
 //November 6, 2014
 //Assignment: Expressions
+//Wacky
+
+//If I save X milk jugs a week, I can make X snowman before Christmas, which is in X days.
 
 
+var milkJugsWeek = prompt("How many milk jugs do you save a week?");
+var daysTilXmas = prompt("How many days are there until Christmas?");
 
+var daysToWeeks = daysTilXmas/7;
+var snowman = milkJugsWeek * daysToWeeks;
+
+var result = "The number of snowman you can make for Christmas is " + snowman + ".";
+alert(result);
