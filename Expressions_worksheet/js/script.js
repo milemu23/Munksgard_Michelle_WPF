e26c26d1855c33dbd63ea97690ecb7e49d19d36b
Michelle Munksgard <design@michellemunksgard.com>
2014-11-06 21:22:03 -0500

    Dog Years

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,12 @@
+//Michelle Munksgard
+//11/06/2014
+//Expressions Worksheet
+
+//Dog Years
+//Determine Sparky's age in dog years
+var humanYears = 3;
+var dogYears = humanYears * 7;
+
+
+console.log("Sparky is " + humanYears + " years old which is " + dogYears + " in dog years." );
+
