096c01b0b9d6841686ea8789e5fe792b892a3f0d
Michelle Munksgard <design@michellemunksgard.com>
2014-11-06 21:22:27 -0500

    Slice of Pie Part 1

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -10,3 +10,17 @@ var dogYears = humanYears * 7;
 
 console.log("Sparky is " + humanYears + " years old which is " + dogYears + " in dog years." );
 
+
+//Slice of Pie Part 1
+//Determine slices of pie per person
+
+var slicesPerPizza = 8;
+var noOfPeople = 20;
+var noOfPizzas = 5;
+var slicesPerPerson;
+
+slicesPerPerson = (noOfPizzas * slicesPerPizza) / noOfPeople;
+
+console.log("Each person ate " + slicesPerPerson + " slices of pizza at the party.");
+
+
