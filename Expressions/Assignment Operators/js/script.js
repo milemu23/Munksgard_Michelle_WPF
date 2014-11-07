c45d98664fa0aef6db494e69460595c0fa7b9e9e
Michelle Munksgard <design@michellemunksgard.com>
2014-11-06 20:56:20 -0500

    Assignment Operators

---
+++ Expressions/Assignment Operators/js/script.js
@@ -0,0 +1,29 @@
+//Assignment Operators
+
+var a = 3;
+a = a + 4;
+console.log(a);
+
+var a = 3;
+a += 4; //same as a = a + 4 - combines operators
+console.log(a);
+
+var a = 3;
+a *= 4;
+console.log(a);
+
+var a = 3;
+a /= 4;
+console.log(a);
+
+var a = 3;
+a -= 4;
+console.log(a);
+
+var a = 3;
+a ++; //increment = same as a = a + 1 // a += 1
+console.log(a);
+
+var a = 3;
+a --;
+console.log(a);
