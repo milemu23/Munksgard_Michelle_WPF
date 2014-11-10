0b6f011137b351876a23d0cefabf67ccff6a74ad
Michelle Munksgard <design@michellemunksgard.com>
2014-11-10 16:03:26 -0500

    Relational Conditionals

---
+++ Conditionals/Relational Expressions/js/script.js
@@ -0,0 +1,11 @@
+//Relational Expressions
+
+var kidHeight = 40;
+var minHeight = 48;
+
+//if the kid is over 48 inches in height
+//compare variables
+if(kidHeight > minHeight){
+    console.log("You can ride the coasters!");
+}
+
