9b7dfd2bbf9ce3b7f2f70865f61365389819c5bf
Michelle Munksgard <design@michellemunksgard.com>
2014-11-10 16:10:06 -0500

    if and else

---
+++ Conditionals/If and Else/js/script.js
@@ -0,0 +1,11 @@
+//if and else
+
+var kidHeight = 52;
+var minHeight = 48;
+
+if(kidHeight > minHeight){
+    console.log("You can ride the coaster!");
+}else{
+    //code performed if condition is false
+    console.log("Sorry kid, you've got some growing to do first!");
+}
