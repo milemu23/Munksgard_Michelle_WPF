b936d3106a67bb506038f3e46f2998c219b149f8
Michelle Munksgard <design@michellemunksgard.com>
2014-11-10 16:06:01 -0500

    Conditions with Expressions

---
+++ Conditionals/Condition with Expressions/js/script.js
@@ -0,0 +1,13 @@
+//Conditions with Expressions
+
+var kidHeight = 47;
+var minHeight = 48;
+var sneakerLift = 2;
+
+if(kidHeight + 2 > minHeight){
+    console.log("You can ride the coaster!");
+}
+
+if(kidHeight + sneakerLift > minHeight){
+    console.log("You can ride the coaster!");
+}
