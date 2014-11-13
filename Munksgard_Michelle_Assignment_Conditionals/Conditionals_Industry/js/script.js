62492cbc6af18816e21d1ca3e44368a97d51f166
Michelle Munksgard <mlmunksgard@fullsail.edu>
2014-11-13 16:37:14 -0500

    Conditionals_Industry Work in Progress

--- Munksgard_Michelle_Assignment_Conditionals/Conditionals_Industry/js/script.js
+++ Munksgard_Michelle_Assignment_Conditionals/Conditionals_Industry/js/script.js
@@ -2,4 +2,8 @@
 //11/13/2014
 //Conditionals_Industry
 
-
+var monthlyBills = 1500.00;
+var wage = 35.00;
+var monthlyIncome = hours * wage;
+var hours = prompt("How many hours did you work this month?");
+var addExpense = prompt("Amount of additional expenses. If none, enter zero.");
