e8e4574f3f5ec4b59d269f099a37a48ef0397477
Michelle Munksgard <mlmunksgard@fullsail.edu>
2014-11-13 16:40:54 -0500

    Conditionals_Industry added validation

--- Munksgard_Michelle_Assignment_Conditionals/Conditionals_Industry/js/script.js
+++ Munksgard_Michelle_Assignment_Conditionals/Conditionals_Industry/js/script.js
@@ -6,4 +6,13 @@ var monthlyBills = 1500.00;
 var wage = 35.00;
 var monthlyIncome = hours * wage;
 var hours = prompt("How many hours did you work this month?");
+
+if(hours === ""){
+    prompt("Please enter the amount of hours you worked this month. Do not leave blank.");
+}
+
 var addExpense = prompt("Amount of additional expenses. If none, enter zero.");
+
+if(addExpense === ""){
+    prompt("Please enter any additional expenses. Do not leave blank. If none, enter zero.");
+}
