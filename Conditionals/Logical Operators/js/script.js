a54ea375b9a3f9f3abd819a0cb45b2f53578e00a
Michelle Munksgard <design@michellemunksgard.com>
2014-11-13 11:05:58 -0500

    Logical Operators

---
+++ Conditionals/Logical Operators/js/script.js
@@ -0,0 +1,13 @@
+//Logical Operators
+
+var budget = 300;
+var iPhonePrice = 199.99;
+var wonLottery = true;
+
+
+//if the price of the phone is less than our budget AND if our paycheck is over 300
+if(iPhonePrice < budget || wonLottery){
+    console.log("We can buy the phone!");
+}else{
+    console.log("No phone for you!!");
+}
