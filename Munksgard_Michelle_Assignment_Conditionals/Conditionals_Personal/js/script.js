af0a78069617d39e8023dd7ccd57d1acb7d9cc71
Michelle Munksgard <mlmunksgard@fullsail.edu>
2014-11-13 16:29:23 -0500

    Conditionals_Personal Final

--- Munksgard_Michelle_Assignment_Conditionals/Conditionals_Personal/js/script.js
+++ Munksgard_Michelle_Assignment_Conditionals/Conditionals_Personal/js/script.js
@@ -10,12 +10,16 @@ var beer = 3.50;
 var vodka = 4.50;
 
 //prompt the user to see how much money they have to spend
-var money = prompt("How much money do you have to spend?");
+var money = prompt("How much money do you have to spend for drinks?");
+//prompt the user for the amount of drinks that they would like to have
 var quantity = prompt("How many drinks do you want to have?");
 
+//create a variable that calculates the price of drink by the amount the user desires to have
 var totalBeer = beer * quantity;
 var totalVodka = vodka * quantity;
 
+//if there is more money than the price of vodka user can get vodka, if not see if there is more money than the price of beer, otherwise the user gets water
+//Alert the user to what they are able to get
 if(totalVodka <= money) {
     console.log("You have enough money to get a vodka!");
     alert("You have enough money to get a vodka!");
