0ec7738a474c473703b387d4a8d867d836705b26
Michelle Munksgard <mlmunksgard@fullsail.edu>
2014-11-13 16:51:14 -0500

    Conditionals_Industry Final

--- Munksgard_Michelle_Assignment_Conditionals/Conditionals_Industry/js/script.js
+++ Munksgard_Michelle_Assignment_Conditionals/Conditionals_Industry/js/script.js
@@ -9,18 +9,25 @@ var wage = 35.00;
 //prompt the user for the amount of hours that they made this month
 var hours = prompt("How many hours did you work this month?");
 
-//validate the users hours
+//validate the users hours to make sure they don't leave them blank
 if(hours === ""){
     prompt("Please enter the amount of hours you worked this month. Do not leave blank.");
 }
 
+//prompt the user for any additional expense they may have had
 var addExpense = prompt("Amount of additional expenses. If none, enter zero.");
 
-var monthlyIncome = hours * wage;
-
+//validate the amount of additional expenses
 if(addExpense === ""){
     prompt("Please enter any additional expenses. Do not leave blank. If none, enter zero.");
 }
 
+//calculate monthlyIncome by multiplying hours and wage
+var monthlyIncome = hours * wage;
+
+//if your monthly income is greater than or equal to the amount of monthly bills + added expenses then you have enough for the month, if not then you need to make more money.
 result = (monthlyIncome >= monthlyBills + addExpense) ? "You have enough money for the month!" : "You need to make more money this month!";
+
+//Print result to the console and alert the user
+console.log(result);
 alert(result);
