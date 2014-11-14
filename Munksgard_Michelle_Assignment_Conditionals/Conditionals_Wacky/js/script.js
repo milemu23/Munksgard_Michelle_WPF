7caf14c641df1c75035f51e8e8ff1f44cff7fddb
Michelle Munksgard <design@michellemunksgard.com>
2014-11-13 20:25:19 -0500

    Conditionals_Wacky Work in Progress

--- Munksgard_Michelle_Assignment_Conditionals/Conditionals_Wacky/js/script.js
+++ Munksgard_Michelle_Assignment_Conditionals/Conditionals_Wacky/js/script.js
@@ -2,3 +2,15 @@
 //11/13/2014
 //Conditionals_Wacky
 
+var snow = prompt("Is there snow on the ground?");
+
+var snowman;
+var snowType = ["crunchie", "soft", "hard", "packable"];
+
+if(snowType === "crunchie" || snowType === "packable"){
+    console.log("You can make a snowman.");
+}else if (snowType === "soft")
+
+alert(snow == "yes" ? "There is a chance you can ")
+
+
