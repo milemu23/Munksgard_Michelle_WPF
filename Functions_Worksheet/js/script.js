85e8abb27a305c89739feba258e6188b03024046
Michelle Munksgard <design@michellemunksgard.com>
2014-11-16 17:34:38 -0500

    Functions Worksheet in Progress

---
+++ Functions_Worksheet/js/script.js
@@ -0,0 +1,15 @@
+//Functions - Worksheet
+
+//Stung
+
+//Givens
+stings = 8.6666666667;
+weight = 150;
+kill = beeStings(stings,weight);
+
+function beeStings(stings, weight){
+    return stings * weight;
+    console.log("It takes " + kill + "stings to kill this animal.");
+}
+
+beeStings();
