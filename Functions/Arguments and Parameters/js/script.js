e27959858a3a3b3f9497fc1110a5777ee19c20d9
Michelle Munksgard <design@michellemunksgard.com>
2014-11-16 17:05:46 -0500

    Arguments and Parameters

---
+++ Functions/Arguments and Parameters/js/script.js
@@ -0,0 +1,9 @@
+//Functions - Arguments and Parameters
+
+function dogYears(age){
+    var dogYears = age * 7;
+    console.log("Sparky is " + dogYears + " years old.");
+}
+
+dogYears(4);
+dogYears(5);
