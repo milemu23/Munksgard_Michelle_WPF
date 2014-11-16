18095b0fd06aed178fdfb24622169629cd9f9876
Michelle Munksgard <design@michellemunksgard.com>
2014-11-16 17:10:16 -0500

    Returning Values

---
+++ Functions/Returning Values/js/script.js
@@ -0,0 +1,10 @@
+//Functions - Returning Values
+
+var total = calcArea(30, 20);
+
+function calcArea(w, h){
+    var area = w * h;
+    return area; //function spitting the info out
+}
+
+console.log(total);
