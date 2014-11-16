a18d9b8196c854c0c53d98f280310c1daf57faa7
Michelle Munksgard <design@michellemunksgard.com>
2014-11-16 16:21:54 -0500

    Function Invocation

---
+++ Functions/Function Invocation/js/script.js
@@ -0,0 +1,21 @@
+//Functions - Function Execution
+
+function outptMsg(){
+    console.log("Hello World!");
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
+
+//call a function to be used
+calcArea();
+
+outptMsg();
+
+calcArea();
+
+calcArea();
