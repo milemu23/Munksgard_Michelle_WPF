e55977ff6d7507c11f961cf16073d0412d0ad522
Michelle Munksgard <design@michellemunksgard.com>
2014-11-16 22:56:55 -0500

    Functions Worksheet Final

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -3,13 +3,16 @@
 //Stung
 
 //Givens
-var stings = 8.6666666667;
-var weight = 150;
-kill = beeStings(stings,weight);
-console.log("It takes " + kill + "stings to kill this animal.");
 
+//declare variable kill to determine how many bee stings it'll take to kill the animal
+//call beeStings with the given variables, stings per pound and weight of animal
+var kill = beeStings(8.66666667,150);
+
+//create the function with the parameters to be used, stings * weight
 function beeStings(stings, weight){
     return stings * weight;
 }
 
-beeStings(8.6667, 150);
+//print the results to the console
+console.log("It takes " + kill + " stings to kill this animal.");
+
