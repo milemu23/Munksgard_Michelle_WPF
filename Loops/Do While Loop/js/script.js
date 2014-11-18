d44ce1ab65f00cb36f1091462539914bcdae2e3f
Michelle Munksgard <design@michellemunksgard.com>
2014-11-17 20:46:26 -0500

    Do While Loop

---
+++ Loops/Do While Loop/js/script.js
@@ -0,0 +1,18 @@
+//While Loops
+
+var b = 50; //setups the index
+
+while(b > 0){ //checks the condition
+    console.log(b + " kegs on the wall.");
+    b--; //increments or decrements the index
+}
+
+//Do While Loops
+
+var c = 10;
+
+//run this code (do) while the condition is false
+do{
+    console.log(c + " kegs on the wall.");
+    c--;
+}while (c > 0);
