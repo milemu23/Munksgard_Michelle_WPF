9d74d5a7083db80cc2e0bb52f69b222338b7b902
Michelle Munksgard <mlmunksgard@fullsail.edu>
2014-11-18 22:04:40 -0500

    Functions_Personal Added Comments

--- Munksgard_Michelle_Assignment_Functions/Functions_Personal/js/script.js
+++ Munksgard_Michelle_Assignment_Functions/Functions_Personal/js/script.js
@@ -7,18 +7,26 @@
 //Assume the average weight loss a week is 2 lbs
 
 //Givens
+//prompt to find out the users current weight
 var currentWeight = prompt("What is your current weight?");
+//prompt to find out the users goal weight
 var goalWeight = prompt("What is your goal weight?");
+//average lbs per week a person can lose in a healthy way
 var lbsPerWeek = 2;
+//total weeks left to reach goal
 var totalWeeksLeft = (currentWeight - goalWeight) / lbsPerWeek;
 
+//declare function result with parameters current weight, goal weight and lbs per week, if current weight - goal weight divided by lbsPerWeek is less than or equal to 0, then the user has reached their goal weight, otherwise the user had this amount of time left if they loss the average 2 lbs per week
 function result(c, g, l){
     if((c - g) / l <= 0){
+        //alert the user if they met their goal
         alert("Congratulations you met your goal!");
     }else{
+        //alert the user telling them how many weeks they have left
         alert("You have about " + totalWeeksLeft + " weeks left to reach your goal. Keep going!" );
     }
 
 }
 
+//call the function
 result(currentWeight, goalWeight, lbsPerWeek);
