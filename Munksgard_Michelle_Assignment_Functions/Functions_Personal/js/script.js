77f956cfbdbea197030efe6dc2681bb3fb66b43b
Michelle Munksgard <mlmunksgard@fullsail.edu>
2014-11-18 21:49:10 -0500

    Functions_Personal Added calcuation

--- Munksgard_Michelle_Assignment_Functions/Functions_Personal/js/script.js
+++ Munksgard_Michelle_Assignment_Functions/Functions_Personal/js/script.js
@@ -7,12 +7,17 @@
 //Assume the average weight loss a week is 2 lbs
 
 //Givens
-var currentWeight = 250;
-var goalWeight = 200;
-var daysInWeek = 7;
+var currentWeight = prompt("What is your current weight?");
+var goalWeight = prompt("What is your goal weight?");
+var totalWeeksLeft = (currentWeight - goalWeight) / 2;
 
-var weightLossDuration(currentWeight > 200){
-    currentWeight--;
-    
+function result(c, g){
+    if(c - g <= 0){
+        alert("Congratulations you met your goal!");
+    }else{
+        alert("You have about " + totalWeeksLeft + " weeks left to reach your goal. Keep going!" );
+    }
+
 }
 
+result(currentWeight, goalWeight);
