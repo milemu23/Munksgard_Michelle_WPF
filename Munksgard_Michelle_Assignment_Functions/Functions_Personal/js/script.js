5467bf37b4194a0f75bffc8ac4d1c53160879b7c
Michelle Munksgard <mlmunksgard@fullsail.edu>
2014-11-18 21:16:18 -0500

    Functions_Personal WIP

--- Munksgard_Michelle_Assignment_Functions/Functions_Personal/js/script.js
+++ Munksgard_Michelle_Assignment_Functions/Functions_Personal/js/script.js
@@ -1,3 +1,18 @@
 //Michelle Munksgard
 //11/18/2014
 //Functions_Personal
+
+//Weight Goal
+//Determine how many days it will take to lose weight to meet the goal
+//Assume the average weight loss a week is 2 lbs
+
+//Givens
+var currentWeight = 250;
+var goalWeight = 200;
+var daysInWeek = 7;
+
+var weightLossDuration(currentWeight > 200){
+    currentWeight--;
+    
+}
+
