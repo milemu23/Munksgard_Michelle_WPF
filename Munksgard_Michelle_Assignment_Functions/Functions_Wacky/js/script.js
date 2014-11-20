15e70f31cf91a5efce845aaa6f8f6beb039e427f
Michelle Munksgard <mlmunksgard@fullsail.edu>
2014-11-20 08:26:38 -0500

    Functions_Wacky Troubleshooting

--- Munksgard_Michelle_Assignment_Functions/Functions_Wacky/js/script.js
+++ Munksgard_Michelle_Assignment_Functions/Functions_Wacky/js/script.js
@@ -8,17 +8,21 @@
 var month = prompt("What month is it? Enter using a number. Ex. November is 11.");
 var currentDate = prompt("What is today's date? Do not include month");
 var daysInNov = 30;
-var daysInDec = 25; // days until Chrismas in December
+var daysInDec = 25; // days until Christmas in December
+var daysLeft;
+var monthsLeft;
 
-function xmas(m, c){
+xmas(month);
+
+function xmas(m){
     if(m === 11){
-        monthsLeft = 12 - m;
-        daysLeft = daysInNov - c + daysInDec;
+        monthsLeft = 12 - month;
+        daysLeft = daysInNov - currentDate + daysInDec;
 
         alert("There is " + monthsLeft + " month and " + daysLeft + " days until Christmas 2014.");
 
     }else if (m === 12){
-        daysLeft = daysInDec - c;
+        daysLeft = daysInDec - currentDate;
 
         alert("There is " + daysLeft + " days until Christmas 2014.");
 
@@ -27,4 +31,3 @@ function xmas(m, c){
     }
 }
 
-xmas(month, currentDate);
