23ca6258cae8882ab6a253fa889ee1b9b425c829
Michelle Munksgard <mlmunksgard@fullsail.edu>
2014-11-20 08:03:21 -0500

    Functions_Industry Layout all Data

--- Munksgard_Michelle_Assignment_Functions/Functions_Industry/js/script.js
+++ Munksgard_Michelle_Assignment_Functions/Functions_Industry/js/script.js
@@ -10,9 +10,7 @@
 var imgHeight = prompt("What is the height of your image? Enter in inches");
 var imgWidth = prompt("What is the width of your image? Enter in inches.");
 
-matSize(prompt("Will your mat have even size sides?"););
 
-function matSize(yes){
     if(true) {
         var evenSides = prompt("What size do you want your mat sides? Enter in inches.");
         frameSizeHeight = imgHeight + evenSides * 2;
@@ -23,7 +21,8 @@ function matSize(yes){
         var matSizeBottom = prompt("What size do you want at the bottom for your mat? Enter in inches");
         var matSizeSides = prompt("What size do you want at the sides for your mat? Enter in inches");
 
+        frameSizeHeight = imgHeight + matSizeTop + matSizeBottom;
+        frameSizeWidth = imgWidth + matSizeSides * 2;
 
-
+        alert("The inside dimensions for your frame are " + frameSizeHeight + " x " + frameSizeWidth + ".");
     }
-}
