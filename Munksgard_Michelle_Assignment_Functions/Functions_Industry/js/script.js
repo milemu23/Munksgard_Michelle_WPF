b9eb3aad571122ed3c354392c687b686747b4d55
Michelle Munksgard <mlmunksgard@fullsail.edu>
2014-11-20 08:13:22 -0500

    Functions_Industry Troubleshooting

--- Munksgard_Michelle_Assignment_Functions/Functions_Industry/js/script.js
+++ Munksgard_Michelle_Assignment_Functions/Functions_Industry/js/script.js
@@ -14,9 +14,9 @@ var evenSides = prompt("Do you want all four sides to be the same?");
 
 
     if(evenSides === "yes") {
-        var evenSides = prompt("What size do you want your mat sides? Enter in inches.");
-        frameSizeHeight = imgHeight + evenSides * 2;
-        frameSizeWidth = imgWidth + evenSides * 2;
+        var sides = prompt("What size do you want your mat sides? Enter in inches.");
+        frameSizeHeight = imgHeight + sides * 2;
+        frameSizeWidth = imgWidth + sides * 2;
         alert("The inside dimensions for your frame are " + frameSizeHeight + " x " + frameSizeWidth + "." );
     }else {
         var matSizeTop = prompt("What size do you want at the top for your mat? Enter in inches.");
