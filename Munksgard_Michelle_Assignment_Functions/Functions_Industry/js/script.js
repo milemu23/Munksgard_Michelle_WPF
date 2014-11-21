727ed3a735f898bb271d2329891b4da47dce7fe8
Michelle Munksgard <mlmunksgard@fullsail.edu>
2014-11-20 21:58:37 -0500

    Added Number objects

--- Munksgard_Michelle_Assignment_Functions/Functions_Industry/js/script.js
+++ Munksgard_Michelle_Assignment_Functions/Functions_Industry/js/script.js
@@ -7,24 +7,33 @@
 
 
 //Givens
-var imgHeight = prompt("What is the height of your image? Enter in inches");
+var imgHeight = prompt("What is the height of your image? Enter in inches.");
+
+while(isNaN(imgHeight) || imgHeight===""){
+    prompt("What is the height of your image? Enter in inches.");
+}
+
 var imgWidth = prompt("What is the width of your image? Enter in inches.");
+
+while(isNaN(imgHeight) || imgHeight===""){
+    prompt("What is the height of your image? Enter in inches.");
+}
 
 var evenSides = prompt("Do you want all four sides to be the same?");
 
 
     if(evenSides === "yes") {
         var sides = prompt("What size do you want your mat sides? Enter in inches.");
-        frameSizeHeight = imgHeight + sides * 2;
-        frameSizeWidth = imgWidth + sides * 2;
+        frameSizeHeight = Number(imgHeight) + Number(sides * 2);
+        frameSizeWidth = Number(imgWidth) + Number(sides * 2);
         alert("The inside dimensions for your frame are " + frameSizeHeight + " x " + frameSizeWidth + "." );
     }else {
         var matSizeTop = prompt("What size do you want at the top for your mat? Enter in inches.");
         var matSizeBottom = prompt("What size do you want at the bottom for your mat? Enter in inches");
         var matSizeSides = prompt("What size do you want at the sides for your mat? Enter in inches");
 
-        frameSizeHeight = imgHeight + matSizeTop + matSizeBottom;
-        frameSizeWidth = imgWidth + matSizeSides * 2;
+        frameSizeHeight = Number(imgHeight) + Number(matSizeTop) + Number(matSizeBottom);
+        frameSizeWidth = Number(imgWidth) + Number(matSizeSides * 2);
 
         alert("The inside dimensions for your frame are " + frameSizeHeight + " x " + frameSizeWidth + ".");
     }
