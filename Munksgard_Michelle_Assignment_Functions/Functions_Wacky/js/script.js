54261c7e99148c69da0521c5aec51d15b849d6a8
Michelle Munksgard <mlmunksgard@fullsail.edu>
2014-11-20 21:33:34 -0500

    Added Loop to Validate and Correcting calculation

--- Munksgard_Michelle_Assignment_Functions/Functions_Wacky/js/script.js
+++ Munksgard_Michelle_Assignment_Functions/Functions_Wacky/js/script.js
@@ -5,31 +5,37 @@
 //Determine days until Christmas
 
 //Givens
+
 var month = prompt("What month is it? Enter using a number. Ex. November is 11.");
 
-while(isNan(month) || min===""){
-    var month = prompt("What month is it? Enter using a number. Ex. November is 11.");
+while(isNaN(month) || month===""){
+    month = prompt("What month is it? Enter using a number. Ex. November is 11.");
 }
 
-
-
 var currentDate = prompt("What is today's date? Do not include month");
+
+//validate prompt and make sure it's a number
+while(isNaN(currentDate) || currentDate===""){
+    currentDate = prompt("What is today's date? Do not include month");
+}
+
 var daysInNov = 30;
 var daysInDec = 25; // days until Christmas in December
 var daysLeft;
 var monthsLeft;
 
-xmas(month);
 
-function xmas(m){
+xmas(month, currentDate);
+
+function xmas(m, d){
     if(m === 11){
         monthsLeft = 12 - month;
-        daysLeft = daysInNov - currentDate + daysInDec;
+        daysLeft = daysInNov - d + daysInDec;
 
         alert("There is " + monthsLeft + " month and " + daysLeft + " days until Christmas 2014.");
 
     }else if (m === 12){
-        daysLeft = daysInDec - currentDate;
+        daysLeft = daysInDec - d;
 
         alert("There is " + daysLeft + " days until Christmas 2014.");
 
@@ -37,4 +43,3 @@ function xmas(m){
         alert("Christmas is over for 2014, please check back later.");
     }
 }
-
