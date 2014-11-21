e3ccf97b6089857a5362e5d47c27f8c2c9cdbd55
Michelle Munksgard <mlmunksgard@fullsail.edu>
2014-11-20 22:35:05 -0500

    Personal Final

--- Munksgard_Michelle_Assignment_Functions/Functions_Personal/js/script.js
+++ Munksgard_Michelle_Assignment_Functions/Functions_Personal/js/script.js
@@ -11,16 +11,16 @@
 var currentWeight = prompt("What is your current weight?");
 
 //validate the users current weight
-if(currentWeight === ""){
-    prompt("Please enter your current weight. Do not leave blank.");
+while(currentWeight === ""){
+    currentWeight = prompt("Please enter your current weight. Do not leave blank.");
 }
 
 //prompt to find out the users goal weight
 var goalWeight = prompt("What is your goal weight?");
 
 //validate the users goal weight
-if(quantity === ""){
-    prompt("Please enter your goal weight. Do not leave blank.");
+while(goalWeight === ""){
+    goalWeight = prompt("Please enter your goal weight. Do not leave blank.");
 }
 
 //average lbs per week a person can lose in a healthy way
@@ -30,14 +30,11 @@ var totalWeeksLeft = (currentWeight - goalWeight) / lbsPerWeek;
 
 //declare function result with parameters current weight, goal weight and lbs per week, if current weight - goal weight divided by lbsPerWeek is less than or equal to 0, then the user has reached their goal weight, otherwise the user had this amount of time left if they loss the average 2 lbs per week
 function result(c, g, l){
-    if((c - g) / l <= 0){
-        //alert the user if they met their goal
-        alert("Congratulations you met your goal!");
-    }else{
-        //alert the user telling them how many weeks they have left
-        alert("You have about " + totalWeeksLeft + " weeks left to reach your goal. Keep going!" );
+if((c - g) / l <= 0) {
+    alert("Congratulations you met your goal!");
+}else {
+    alert("You have about " + totalWeeksLeft + " weeks left to reach your goal. Keep going!" );
     }
-
 }
 
 //call the function
